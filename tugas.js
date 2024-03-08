6150de1e729ed045c461337c67ec593e372cf238
shalmar14 <omar.imanie03@gmail.com>
2024-03-08 10:28:14 +0700

    first commit

--- tugas.js
+++ tugas.js
@@ -1,14 +1,15 @@
+function findCountry(nameofCountry) {
+    const countries = ['Indonesia', 'Malaysia', 'Singapura', 'Brunei', 'Filipina', 'Kamboja', 'Amerika Serikat', 'Inggris', 'Timor Leste'];
 
+    const found = countries.filter((e) => e.toUpperCase() === nameofCountry.toUpperCase());
 
-function country() {
-const countries = ['Indonesia', 'Malaysia', 'Singapura', 'Brunei', 'Filipina', 'Kamboja', 'Amerika Serikat', 'Inggris', 'Timor Leste']
+    if (found) {
+        console.log("Ada");
+    } else {
+        console.log("Tidak Ada");
+    }
 
-if (countries.filter((countries) => countries.includes('a', 'o', 's'))) {
-console.log('Ada');
-} else {
-console.log('Tidak Ada');
+    return(`Negara yang diinput : ${nameofCountry}`);
 }
 
-}
-
-country();
+console.log(findCountry("Indo"));
